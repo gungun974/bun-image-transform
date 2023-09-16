4fb233da3f7f0ba17937da34d0ad592ae36e5abe
Gungun974 <xfelix974@gmail.com>
2023-09-16 22:38:24 +0400

    Add unit test for plugin image import transformation

--- tests/plugin.test.ts
+++ tests/plugin.test.ts
@@ -19,6 +19,28 @@ plugin(
   }),
 );
 
+describe("import", () => {
+  it("should import image and return a link to generated image", async () => {
+    const { default: image }: any = await import("./bun-logo.png?&bunimg");
+    const originalFile = Bun.file(resolve(import.meta.dir, "./bun-logo.png"));
+    const generatePath = image.replace("file:", "");
+    const newFile = Bun.file(generatePath);
+
+    expect(generatePath).not.toBe(resolve(import.meta.dir, "./bun-logo.png"));
+    expect(Bun.hash(await newFile.arrayBuffer())).not.toBe(
+      Bun.hash(await originalFile.arrayBuffer()),
+    );
+  });
+
+  it("should not import image and generate when image don't have the `&bunimg` suffix", async () => {
+    const { default: image }: any = await import("./bun-logo.png");
+
+    expect(image.replace("file:", "")).toBe(
+      resolve(import.meta.dir, "./bun-logo.png"),
+    );
+  });
+});
+
 describe("resize", () => {
   it("should resize bun logo with a width of 16px", async () => {
     const { default: image }: any = await import(
